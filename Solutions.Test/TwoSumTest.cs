eb16525664e32c10519be158fa27b0b46c5fd847
Aydan <91035120+Aydanjb@users.noreply.github.com>
2025-01-06 17:54:55 -0500

    Completed Leetcode 1.

---
+++ Solutions.Test/TwoSumTest.cs
@@ -0,0 +1,49 @@
+﻿using JetBrains.Annotations;
+using Solutions;
+
+namespace Solutions.Test;
+
+[TestSubject(typeof(Solution1))]
+public class TwoSumTest
+{
+
+    [Fact]
+    public void TestTwoSumExample1()
+    {
+        var solution = new Solution1();
+        int[] expected = [0, 1];
+        int[] actual = solution.TwoSum([2, 7, 11, 15], 9);
+        
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void TestTwoSumExample2()
+    {
+        var solution = new Solution1();
+        int[] expected = [1, 2];
+        int[] actual = solution.TwoSum([3, 2, 4], 6);
+        
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void TestTwoSumExample3()
+    {
+        var solution = new Solution1();
+        int[] expected = [0, 1];
+        int[] actual = solution.TwoSum([3, 3], 6);
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void TestTwoSumEdgeCase1()
+    {
+        var solution = new Solution1();
+        int[] expected = [2, 4];
+        int[] actual = solution.TwoSum([-1, -2, -3, -4, -5], -8);
+        
+        Assert.Equal(expected, actual);
+    }
+}
---
+++ Solutions/TwoSum.cs
@@ -0,0 +1,16 @@
+﻿namespace Solutions;
+
+public class Solution1
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        Dictionary<int, int> indices = [];
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (indices.TryGetValue(nums[i], out var index)) return [index, i];
+            indices.TryAdd(target - nums[i], i);
+        }
+
+        return [];
+    }
+}
