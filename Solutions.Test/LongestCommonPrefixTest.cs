1ef81918738efaca77099534959b9b5595c65c10
Aydan <91035120+Aydanjb@users.noreply.github.com>
2025-01-06 20:43:55 -0500

    Completed Leetcode 14.

---
+++ Solutions.Test/LongestCommonPrefixTest.cs
@@ -0,0 +1,29 @@
+﻿using JetBrains.Annotations;
+using Solutions;
+
+namespace Solutions.Test;
+
+[TestSubject(typeof(Solution14))]
+public class LongestCommonPrefixTest
+{
+
+    [Fact]
+    public void TestLongestCommonPrefixExample1()
+    {
+        var solution = new Solution14();
+        string expected = "fl";
+        string actual = solution.LongestCommonPrefix(["flower", "flow", "flight"]);
+        
+        Assert.Equal(expected, actual);
+    }
+    
+    [Fact]
+    public void TestLongestCommonPrefixExample2()
+    {
+        var solution = new Solution14();
+        string expected = "";
+        string actual = solution.LongestCommonPrefix(["dog", "racecar", "car"]);
+        
+        Assert.Equal(expected, actual);
+    }
+}
---
+++ Solutions/LongestCommonPrefix.cs
@@ -0,0 +1,22 @@
+﻿namespace Solutions;
+
+public class Solution14
+{
+    public string LongestCommonPrefix(string[] strs)
+    {
+        int n = strs[0].Length;
+        string result = "";
+
+        for (int i = 0; i < n; i++)
+        {
+            foreach (var s in strs)
+            {
+                if (i == s.Length || s[i] != strs[0][i]) return result;
+            }
+
+            result += strs[0][i];
+        }
+
+        return result;
+    }
+}
